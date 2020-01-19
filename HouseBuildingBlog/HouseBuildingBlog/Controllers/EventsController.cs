49126fd4694799bde7ddd874bd0ce87f2f89d918
Matthias Eggert <eckenblitz@msn.com>
2020-01-19 10:47:55 +0100

    Initial Service

---
+++ HouseBuildingBlog/HouseBuildingBlog/Controllers/EventsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HouseBuildingBlog.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventsController : ControllerBase
+    {
+        private readonly ILogger<EventsController> _logger;
+
+        public EventsController(ILogger<EventsController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
