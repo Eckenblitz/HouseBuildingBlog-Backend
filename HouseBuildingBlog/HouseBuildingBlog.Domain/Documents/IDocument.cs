fa8c179a508f2a0fd1200b10292a880b1b66927f
Matthias Eggert <eckenblitz@msn.com>
2020-10-15 13:07:38 +0200

    started documents

---
+++ HouseBuildingBlog/HouseBuildingBlog.Domain/Documents/IDocument.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HouseBuildingBlog.Domain.Documents
+{
+	public interface IDocument
+	{
+		Guid DocumentId { get; }
+		string Title { get; }
+		string Comment { get; }
+		string FileAdress { get; }
+	}
+}
---
+++ HouseBuildingBlog/HouseBuildingBlog/Documents/Models/Document.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HouseBuildingBlog.Domain.Documents;
+
+namespace HouseBuildingBlog.Api.Documents.Models
+{
+	public class Document : IDocument
+	{
+		public Guid DocumentId { get; private set; }
+
+		public string Title { get; private set; }
+
+		public string Comment { get; private set; }
+
+		public string FileAdress { get; private set; }
+
+		public Document() { }
+
+		public Document(IDocument doc) {
+			this.DocumentId = doc.DocumentId;
+			this.Title = doc.Title;
+			this.Comment = doc.Comment;
+			this.FileAdress = doc.FileAdress;
+		}
+	}
+}
