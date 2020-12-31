cc978e71bb9bd2c04ebdfa7b5881e7b06647b7d3
Matthias Eggert <eckenblitz@msn.com>
2020-12-31 14:11:47 +0100

    add database version

--- HouseBuildingBlog/HouseBuildingBlog.Persistence.MSSql/Scripts/CreateDatabaseScript.sql
+++ HouseBuildingBlog/HouseBuildingBlog.Persistence.MSSql/Scripts/CreateDatabaseScript.sql
@@ -1,2 +1,18 @@
 ﻿CREATE DATABASE HouseBuildingBlog
 GO
+
+USE HouseBuildingBlog;
+GO
+
+CREATE SCHEMA Info
+GO
+
+CREATE TABLE Info.DatabaseVersion(
+	Major int NOT NULL,
+	Minor int NOT NULL,
+	Patch int NOT NULL
+)
+GO
+
+INSERT INTO Info.DatabaseVersion VALUES (0, 0, 0)
+GO
