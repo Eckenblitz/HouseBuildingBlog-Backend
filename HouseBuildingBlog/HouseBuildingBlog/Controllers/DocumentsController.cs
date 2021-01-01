c4efed9e7d47d3c6d9a8533236104705c69a82b2
Matthias Eggert <eckenblitz@msn.com>
2021-01-01 21:49:41 +0100

    CR: add response types

--- HouseBuildingBlog/HouseBuildingBlog/Controllers/DocumentsController.cs
+++ HouseBuildingBlog/HouseBuildingBlog/Controllers/DocumentsController.cs
@@ -6,6 +6,7 @@ using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace HouseBuildingBlog.Api.Controllers
@@ -23,34 +24,42 @@ namespace HouseBuildingBlog.Api.Controllers
 
 		[HttpPost]
 		[ProducesResponseType(typeof(DocumentQueryDto), StatusCodes.Status201Created)]
+		[ProducesResponseType(typeof(BadRequestObjectResult), StatusCodes.Status400BadRequest)]
 		public async Task<IActionResult> CreateDocument([FromBody] DocumentCommandDto dto)
 		{
 			return await _mediator.Send(new CreateDocumentCommand(dto));
 		}
 
 		[HttpGet]
+		[ProducesResponseType(typeof(IEnumerable<DocumentQueryDto>), StatusCodes.Status200OK)]
 		public async Task<IActionResult> GetAllDocuments()
 		{
 			return await _mediator.Send(new GetAllDocumentsQuery());
 		}
 
 		[HttpGet("{id}")]
+		[ProducesResponseType(typeof(DocumentQueryDto), StatusCodes.Status200OK)]
+		[ProducesResponseType(typeof(NotFoundObjectResult), StatusCodes.Status404NotFound)]
 		public async Task<IActionResult> GetSingleDocument(Guid id)
 		{
 			return await _mediator.Send(new GetSingleDocumentQuery(id));
 		}
 
 		[HttpPut("{id}")]
+		[ProducesResponseType(typeof(DocumentQueryDto), StatusCodes.Status200OK)]
+		[ProducesResponseType(typeof(NotFoundObjectResult), StatusCodes.Status404NotFound)]
+		[ProducesResponseType(typeof(BadRequestObjectResult), StatusCodes.Status400BadRequest)]
 		public async Task<IActionResult> UpdateDocument(Guid id, [FromBody] DocumentCommandDto dto)
 		{
 			return await _mediator.Send(new UpdateDocumentCommand(id, dto));
 		}
 
 		[HttpDelete("{id}")]
+		[ProducesResponseType(typeof(DocumentQueryDto), StatusCodes.Status200OK)]
+		[ProducesResponseType(typeof(NotFoundObjectResult), StatusCodes.Status404NotFound)]
 		public async Task<IActionResult> DeleteDocument(Guid id)
 		{
 			return await _mediator.Send(new DeleteDocumentCommand(id));
-
 		}
 	}
 }
