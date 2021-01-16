e017be476da2f4b63293da52255093bbd2362503
Matthias Eggert <eckenblitz@msn.com>
2021-01-16 23:43:21 +0100

    Tests for Documents in Api project

---
+++ HouseBuildingBlog/HouseBuildingBlog.Api.Tests/Controllers/DocumentsControllerTests.cs
@@ -0,0 +1,108 @@
+﻿using HouseBuildingBlog.Api.Controllers;
+using HouseBuildingBlog.Api.Documents.Commands;
+using HouseBuildingBlog.Api.Documents.Commands.Contracts;
+using HouseBuildingBlog.Api.Documents.Queries;
+using MediatR;
+using NSubstitute;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HouseBuildingBlog.Api.Tests.Controllers
+{
+	public class DocumentsControllerTests
+	{
+		private DocumentsController SuT { get; }
+		private readonly IMediator _mediator;
+
+		public DocumentsControllerTests()
+		{
+			_mediator = Substitute.For<IMediator>();
+			SuT = new DocumentsController(_mediator);
+		}
+
+		[Fact]
+		public async Task Expect_CalledCreateDocumentCommand_When_CreateDocumentIsCalled()
+		{
+			//Arrange
+			var data = new DocumentCommandDto()
+			{
+				Title = "Title",
+				Comment = "Comment",
+				Price = 1.23M,
+				EventId = Guid.NewGuid()
+			};
+
+			//Act
+			await SuT.CreateDocument(data);
+
+			//Assert
+			await _mediator.Received(1).Send(Arg.Is<CreateDocumentCommand>(c =>
+				c.Title == data.Title
+				&& c.Comment == data.Comment
+				&& c.Price == data.Price
+				&& c.EventId == data.EventId));
+		}
+
+		[Fact]
+		public async Task Expect_CalledUpdateDocumentCommand_When_UpdateDocumentIsCalled()
+		{
+			//Arrange
+			var documentId = Guid.NewGuid();
+			var data = new DocumentCommandDto()
+			{
+				Title = "Title",
+				Comment = "Comment",
+				Price = 1.23M,
+				EventId = Guid.NewGuid()
+			};
+
+			//Act
+			await SuT.UpdateDocument(documentId, data);
+
+			//Assert
+			await _mediator.Received(1).Send(Arg.Is<UpdateDocumentCommand>(c =>
+				c.DocumentId == documentId
+				&& c.Title == data.Title
+				&& c.Comment == data.Comment
+				&& c.Price == data.Price
+				&& c.EventId == data.EventId));
+		}
+
+		[Fact]
+		public async Task Expect_CalledDeleteDocumentCommand_When_DeleteDocumentIsCalled()
+		{
+			//Arrange
+			var documentId = Guid.NewGuid();
+
+			//Act
+			await SuT.DeleteDocument(documentId);
+
+			//Assert
+			await _mediator.Received(1).Send(Arg.Is<DeleteDocumentCommand>(c => c.DocumentId == documentId));
+		}
+
+		[Fact]
+		public async Task Expect_CalledGetAllDocumentsQuery_When_GetAllDocumentsIsCalled()
+		{
+			//Arrange / Act
+			await SuT.GetAllDocuments();
+
+			//Assert
+			await _mediator.Received(1).Send(Arg.Any<GetAllDocumentsQuery>());
+		}
+
+		[Fact]
+		public async Task Expect_CalledGetSingleDocumentQuery_When_GetSingleDocumentIsCalled()
+		{
+			//Arrange
+			var documentId = Guid.NewGuid();
+
+			//Act
+			await SuT.GetSingleDocument(documentId);
+
+			//Assert
+			await _mediator.Received(1).Send(Arg.Is<GetSingleDocumentQuery>(q => q.DocumentId == documentId));
+		}
+	}
+}
---
+++ HouseBuildingBlog/HouseBuildingBlog.Api.Tests/Documents/Commands/DeleteDocumentHandlerTests.cs
@@ -0,0 +1,70 @@
+﻿using HouseBuildingBlog.Api.Documents.Commands;
+using HouseBuildingBlog.Api.Documents.Queries;
+using HouseBuildingBlog.Api.Documents.Queries.Contracts;
+using HouseBuildingBlog.Domain.Documents;
+using HouseBuildingBlog.Domain.Errors;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HouseBuildingBlog.Api.Tests.Documents.Commands
+{
+	public class DeleteDocumentHandlerTests : ActionResultTestBase
+	{
+		private DeleteDocumentHandler SuT { get; }
+		private readonly IWriteDocumentsAggregate _writeDocumentsAggregate;
+
+		public DeleteDocumentHandlerTests()
+		{
+			_writeDocumentsAggregate = Substitute.For<IWriteDocumentsAggregate>();
+			SuT = new DeleteDocumentHandler(_writeDocumentsAggregate);
+		}
+
+		[Fact]
+		public async Task Expect_OkResultWithDeletedDocument_When_DocumentWasDeleted()
+		{
+			//Arrange
+			var documentId = Guid.NewGuid();
+			var document = new Document(documentId, new TestDocumentContent()
+			{
+				Title = "Title",
+				Comment = "Comment",
+				Price = 1.23M,
+				EventId = Guid.NewGuid()
+			});
+			var command = new DeleteDocumentCommand(documentId);
+			_writeDocumentsAggregate.DeleteDocumentAsync(Arg.Is<Guid>(documentId)).Returns(document);
+
+			//Act
+			var result = await SuT.Handle(command, CancellationToken.None);
+
+			//Assert
+			CheckResult<OkObjectResult, DocumentQueryDto>(result, d =>
+				d.DocumentId == document.DocumentId
+				&& d.Title == document.Title
+				&& d.Comment == document.Comment
+				&& d.EventId == document.EventId
+				&& d.Price == document.Price);
+		}
+
+		[Fact]
+		public async Task Expect_NotFoundResult_When_DocumentCanNotBeFound()
+		{
+			//Arrange
+			var documentId = Guid.NewGuid();
+			var command = new DeleteDocumentCommand(documentId);
+			_writeDocumentsAggregate.DeleteDocumentAsync(Arg.Is<Guid>(documentId))
+				.Throws(new AggregateNotFoundException("", documentId));
+
+			//Act
+			var result = await SuT.Handle(command, CancellationToken.None);
+
+			//Assert
+			_ = CheckResult<NotFoundObjectResult>(result);
+		}
+	}
+}
