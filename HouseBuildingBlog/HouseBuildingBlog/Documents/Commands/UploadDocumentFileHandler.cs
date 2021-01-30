872240ccd3eb3dd8224f7c1f8e0bd30da089b3f4
Matthias Eggert <eckenblitz@msn.com>
2021-01-30 22:04:14 +0100

    handle domain exceptions on upload document

--- HouseBuildingBlog/HouseBuildingBlog/Documents/Commands/UploadDocumentFileHandler.cs
+++ HouseBuildingBlog/HouseBuildingBlog/Documents/Commands/UploadDocumentFileHandler.cs
@@ -1,5 +1,6 @@
 ﻿using HouseBuildingBlog.Api.Services;
 using HouseBuildingBlog.Domain.Documents;
+using HouseBuildingBlog.Domain.Errors;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading;
@@ -22,7 +23,19 @@ namespace HouseBuildingBlog.Api.Documents.Commands
 		{
 			var file = await _transformFileService.TransformAsync(request.File);
 			//ToDo: validate file? https://stackoverflow.com/questions/56588900/how-to-validate-uploaded-file-in-asp-net-core/56592790
-			_ = await _writeDocumentsAggregate.UploadFileAsync(request.DocumentId, new DocumentFile(request.DocumentId, file));
+			try
+			{
+				_ = await _writeDocumentsAggregate.UploadFileAsync(request.DocumentId, new DocumentFile(request.DocumentId, file));
+			}
+			catch (AggregateNotFoundException ex)
+			{
+				return new NotFoundObjectResult(ex.Error);
+			}
+			catch (ValidationException ex)
+			{
+				return new BadRequestObjectResult(ex.ValidationErrors);
+			}
+
 			//ToDO send with Data?
 			return new OkResult();
 		}
