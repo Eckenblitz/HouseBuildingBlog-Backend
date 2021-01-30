b02067c67dc1fe2147f4307374cb8ce3fee2b12d
Matthias Eggert <eckenblitz@msn.com>
2021-01-30 17:44:15 +0100

    implement ReadDocumentFileRepository

---
+++ HouseBuildingBlog/HouseBuildingBlog.Persistence.MSSql/Documents/ReadDocumentFileRepository.cs
@@ -0,0 +1,26 @@
+﻿using HouseBuildingBlog.Domain.Documents;
+using HouseBuildingBlog.Persistence.MSSql.Files;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace HouseBuildingBlog.Persistence.MSSql.Documents
+{
+	public class ReadDocumentFileRepository : DocumentFileRepositoryBase, IReadFileRepository<IDocumentFile>
+	{
+		public ReadDocumentFileRepository(MSSQLConfig config) : base(config)
+		{ }
+
+		public async Task<byte[]> ReadFileBinariesAsync(IDocumentFile file)
+		{
+			try
+			{
+				return await File.ReadAllBytesAsync(Path.Combine(_fileDirectory, GetFileName(file)));
+			}
+			catch (Exception ex) when (ex is FileNotFoundException || ex is DirectoryNotFoundException)
+			{
+				throw new FileNotFoundException<IDocumentFile>(file, ex);
+			}
+		}
+	}
+}
---
+++ HouseBuildingBlog/HouseBuildingBlog.Persistence.MSSql/Files/FileNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using HouseBuildingBlog.Domain.Files;
+using System;
+
+namespace HouseBuildingBlog.Persistence.MSSql.Files
+{
+	public class FileNotFoundException<TFile> : Exception where TFile : IFile
+	{
+		public TFile File { get; }
+
+		public Exception SystemException { get; }
+
+		public FileNotFoundException(TFile file, Exception ex)
+		{
+			File = file;
+			SystemException = ex;
+		}
+	}
+}
