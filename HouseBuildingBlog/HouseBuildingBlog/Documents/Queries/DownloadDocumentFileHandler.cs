c1db43afb78e75c7e82f71cd301dfcffbfb0cfaa
Matthias Eggert <eckenblitz@msn.com>
2021-02-01 21:15:28 +0100

    remove todo

--- HouseBuildingBlog/HouseBuildingBlog/Documents/Queries/DownloadDocumentFileHandler.cs
+++ HouseBuildingBlog/HouseBuildingBlog/Documents/Queries/DownloadDocumentFileHandler.cs
@@ -10,7 +10,6 @@ using System.Threading.Tasks;
 
 namespace HouseBuildingBlog.Api.Documents.Queries
 {
-	//ToDo: Tests
 	public class DownloadDocumentFileHandler : IRequestHandler<DownloadDocumentFileQuery, IActionResult>
 	{
 		private readonly IReadDocumentsAggregate _readDocumentsAggregate;
