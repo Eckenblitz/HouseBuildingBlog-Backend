62d5409dda5e1db35d8e720888f195633a764881
Matthias Eggert <m.eggert@cid.com>
2021-02-17 22:31:59 +0100

    dockerize backend

--- HouseBuildingBlog/HouseBuildingBlog.Persistence.MSSql/MSSQLConfig.cs
+++ HouseBuildingBlog/HouseBuildingBlog.Persistence.MSSql/MSSQLConfig.cs
@@ -1,17 +1,19 @@
 ﻿namespace HouseBuildingBlog.Persistence.MSSql
 {
-	public class MSSQLConfig
-	{
-		public string Server { get; set; }
+    public class MSSQLConfig
+    {
+        public string Server { get; set; }
 
-		public string DatabaseName { get; set; }
+        public int Port { get; set; }
 
-		public string UserId { get; set; }
+        public string DatabaseName { get; set; }
 
-		public string UserPw { get; set; }
+        public string UserId { get; set; }
 
-		public bool TrustedConnection { get; set; }
+        public string UserPw { get; set; }
 
-		public string FileStorageLocation { get; set; }
-	}
+        public bool TrustedConnection { get; set; }
+
+        public string FileStorageLocation { get; set; }
+    }
 }
