baec109f6c2d835a639af34e85d6cefc747e8a83
Matthias Eggert <eckenblitz@msn.com>
2021-03-03 22:39:28 +0100

    Basic Domain interfaces for Images and Galleries

---
+++ HouseBuildingBlog/HouseBuildingBlog.Domain/Images/IGallery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HouseBuildingBlog.Domain.Images
+{
+	public interface IGallery
+	{
+		Guid GalleryId { get; }
+
+		string Title { get; }
+
+		IEnumerable<Guid> ImageIds { get; }
+	}
+}
---
+++ HouseBuildingBlog/HouseBuildingBlog.Domain/Images/IImage.cs
@@ -0,0 +1,14 @@
+﻿using HouseBuildingBlog.Domain.Files;
+using System;
+
+namespace HouseBuildingBlog.Domain.Images
+{
+	public interface IImage : IFile
+	{
+		Guid ImageId { get; }
+
+		string Title { get; }
+
+		string Comment { get; }
+	}
+}
