4e797cecd723a6cc91ac9f4089a9a0009daaa7a8
Matthias Eggert <eckenblitz@msn.com>
2021-03-03 23:14:26 +0100

    Testclass for Gallery

---
+++ HouseBuildingBlog/HouseBuildingBlog.Domain.TestBase/Images/TestGallery.cs
@@ -0,0 +1,23 @@
+﻿using HouseBuildingBlog.Domain.Images;
+using System;
+using System.Collections.Generic;
+
+namespace HouseBuildingBlog.Domain.TestBase.Images
+{
+	public class TestGallery : IGallery
+	{
+		public Guid GalleryId { get; set; }
+
+		public string Title { get; set; }
+
+		public IEnumerable<Guid> ImageIds { get; set; }
+
+		public TestGallery() { }
+
+		public TestGallery(Guid galleryId, string title)
+		{
+			GalleryId = galleryId;
+			Title = title;
+		}
+	}
+}
