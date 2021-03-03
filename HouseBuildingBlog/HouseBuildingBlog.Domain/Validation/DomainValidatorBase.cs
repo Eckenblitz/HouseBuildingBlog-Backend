4f053c8164fa1a442d95edfc650bd78bce147169
Matthias Eggert <eckenblitz@msn.com>
2021-03-03 23:13:54 +0100

    base class for Validation

---
+++ HouseBuildingBlog/HouseBuildingBlog.Domain/Validation/DomainValidatorBase.cs
@@ -0,0 +1,20 @@
+﻿using HouseBuildingBlog.Domain.Errors;
+using System.Collections.Generic;
+
+namespace HouseBuildingBlog.Domain.Validation
+{
+	public abstract class DomainValidatorBase
+	{
+		protected ICollection<DomainError> Errors { get; }
+
+		protected DomainValidatorBase()
+		{
+			Errors = new List<DomainError>();
+		}
+
+		protected void AddError(string errorCode, params (string key, string value)[] parameters)
+		{
+			Errors.Add(new DomainError(errorCode, parameters));
+		}
+	}
+}
