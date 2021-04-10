9be84325523a0750c1ad6cdda87ee19f1f35e900
Matthias Eggert <eckenblitz@msn.com>
2021-04-10 13:54:54 +0200

    AssignDocumentToEventCommand

---
+++ HouseBuildingBlog/HouseBuildingBlog.Domain.TestBase/Events/TestEvent.cs
@@ -0,0 +1,30 @@
+﻿using HouseBuildingBlog.Domain.Events;
+using System;
+using System.Collections.Generic;
+
+namespace HouseBuildingBlog.Domain.TestBase.Events
+{
+	public class TestEvent : IEvent
+	{
+		public Guid EventId { get; set; }
+
+		public string Title { get; set; }
+
+		public DateTime Date { get; set; }
+
+		public IEnumerable<Guid> TagIds { get; set; }
+
+		public string Description { get; set; }
+
+		public TestEvent() { }
+
+		public TestEvent(IEvent @event)
+		{
+			EventId = @event.EventId;
+			Title = @event.Title;
+			Date = @event.Date;
+			TagIds = @event.TagIds != null ? new List<Guid>(@event.TagIds) : new List<Guid>();
+			Description = @event.Description;
+		}
+	}
+}
---
+++ HouseBuildingBlog/HouseBuildingBlog/Documents/Commands/AssignDocumentToEventCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace HouseBuildingBlog.Api.Documents.Commands
+{
+	public class AssignDocumentToEventCommand : IRequest<IActionResult>
+	{
+		public Guid DocumentId { get; }
+
+		public Guid EventId { get; }
+
+		public AssignDocumentToEventCommand(Guid documentId, Guid eventId)
+		{
+			DocumentId = documentId;
+			EventId = eventId;
+		}
+	}
+}
