fd3a5920da9068677eb6526b04eb8ec67c7321e4
Matthias Eggert <eckenblitz@msn.com>
2021-04-10 21:17:09 +0200

    Handler: unassign event from document

---
+++ HouseBuildingBlog/HouseBuildingBlog/Documents/Commands/UnassignEventFromDocumentCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace HouseBuildingBlog.Api.Documents.Commands
+{
+	public class UnassignEventFromDocumentCommand : IRequest<IActionResult>
+	{
+		public Guid DocumentId { get; }
+
+		public UnassignEventFromDocumentCommand(Guid documentId)
+		{
+			DocumentId = documentId;
+		}
+	}
+}
