66e853f85e741d7a2784102f7a08bcedca84ba73
Matthias Eggert <m.eggert@cid.com>
2021-04-26 18:03:18 +0200

    removed unused property

--- HouseBuildingBlog/HouseBuildingBlog/Images/Queries/Contracts/GalleryQueryDto.cs
+++ HouseBuildingBlog/HouseBuildingBlog/Images/Queries/Contracts/GalleryQueryDto.cs
@@ -5,18 +5,16 @@ using System.Collections.Generic;
 
 namespace HouseBuildingBlog.Api.Images.Queries.Contracts
 {
-	public class GalleryQueryDto
-	{
-		public Guid GalleryId { get; set; }
+    public class GalleryQueryDto
+    {
+        public Guid GalleryId { get; set; }
 
-		public string Title { get; set; }
+        public string Title { get; set; }
 
-		public string Comment { get; set; }
+        public IEnumerable<ImageQueryDto> Images { get; set; }
 
-		public IEnumerable<ImageQueryDto> Images { get; set; }
+        public SimpleEventQueryDto Event { get; set; }
 
-		public SimpleEventQueryDto Event { get; set; }
-
-		public IEnumerable<TagQueryDto> Tags { get; set; }
-	}
+        public IEnumerable<TagQueryDto> Tags { get; set; }
+    }
 }
